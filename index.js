37e5f74b80b609c7d7e79d70ba14d70d457570f7
Ghadeer Srouji <ghadeersrouji99@gmail.com>
2023-04-14 23:41:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride < 400) {
+    return "This one is on me!";
+  } else if (ride > 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000 && ride < 2500 ) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride => 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  let message = ""
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go."
+  return message
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
 }
+
+}
+
+console.log(switchOnCharmFromTip())
